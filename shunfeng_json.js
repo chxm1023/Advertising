b15bc5264a15470f685f1d6afa1f292b7b1b9f78
chxm1023 <106084065+chxm1023@users.noreply.github.com>
2022-11-29 23:18:47 +0800

    Update shunfeng_json.js

--- shunfeng_json.js
+++ shunfeng_json.js
@@ -1,9 +1,9 @@
 /****************************************
-version     v0.0.1
-updatetime  2022-11-17
-tgchannel   https://t.me/ddgksf2021
-function    顺丰
-author      ddgksf2013
+
+项目名称：顺丰
+使用声明：仅供学习与交流，请勿转载与贩卖！⚠️⚠️⚠️
+
+*****************************************
 
 [rewrite_local]
 
@@ -16,9 +16,9 @@ hostname = ccsp-egmas.sf-express.com
 ****************************************/
 
 if ($request.url.indexOf("app/ad/queryInfoFlow") != -1) {
-    var ddgksf2013 = JSON.parse($response.body);
-    ddgksf2013.obj = Object.values(ddgksf2013.obj).filter((item) => item.adverId==2833);
+    var chxm1023 = JSON.parse($response.body);
+    chxm1023.obj = Object.values(chxm1023.obj).filter((item) => item.adverId==2833);
     $done({
-        body: JSON.stringify(ddgksf2013),
+        body: JSON.stringify(chxm1023),
     });
 }
