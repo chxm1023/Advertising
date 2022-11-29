0155e7590e5ffa7f086aa590b2337aa524ce3647
chxm1023 <106084065+chxm1023@users.noreply.github.com>
2022-11-29 23:14:01 +0800

    Update quting.js

--- quting.js
+++ quting.js
@@ -3,12 +3,13 @@
 项目功能：趣听音乐馆 屏蔽弹窗-自动激活App
 使用声明：仅供学习与交流，请勿转载与贩卖！⚠️⚠️⚠️
 转载来自：墨鱼@ddgksf2013
+
 *****************************************
 
             
 [rewrite_local]
 
-^https?:\/\/api\.bspapp\.com\/client$ url script-response-body https://raw.githubusercontent.com/chxm1023/script/main/Advertising/quting.js
+^https?:\/\/api\.bspapp\.com\/client$ url script-response-body https://raw.githubusercontent.com/chxm1023/Advertising/main/quting.js
 
 [mitm]
 
