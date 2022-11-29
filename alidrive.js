73fddff475537b8d8f61e72982d94bfd4655779e
chxm1023 <106084065+chxm1023@users.noreply.github.com>
2022-11-29 23:09:21 +0800

    Update alidrive.js

--- alidrive.js
+++ alidrive.js
@@ -3,7 +3,7 @@
 [rewrite_local]
 
 # 阿里云盘-优化首页display
-^https?:\/\/api\.aliyundrive\.com\/apps\/v\d\/users\/apps\/widgets$ url script-response-body https://raw.githubusercontent.com/chxm1023/script/main/Advertising/alidrive.js
+^https?:\/\/api\.aliyundrive\.com\/apps\/v\d\/users\/apps\/widgets$ url script-response-body https://raw.githubusercontent.com/chxm1023/Advertising/main/alidrive.js
 
 [mitm]
 
