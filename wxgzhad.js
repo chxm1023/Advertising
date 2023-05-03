ec24f3121e99572b2c34c0811d3cb94fb28332f5
chxm1023 <106084065+chxm1023@users.noreply.github.com>
2023-05-03 10:39:35 +0800

    Update wxgzhad.js

--- wxgzhad.js
+++ wxgzhad.js
@@ -20,7 +20,8 @@ var body = $response.body;
 var chxm1023 = JSON.parse(body);
 
 chxm1023.advertisement_num = 0;
-
 chxm1023.advertisement_info = [];
+delete chxm1023.appid;
 
 $done({body : JSON.stringify(chxm1023)});
+ 
