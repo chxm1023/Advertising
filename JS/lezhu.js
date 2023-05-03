64e63c4f70b7562e72cb1abe0f953e8c96427089
chxm1023 <106084065+chxm1023@users.noreply.github.com>
2023-05-03 11:13:20 +0800

    Delete JS directory

--- JS/lezhu.js
+++
@@ -1,4 +0,0 @@
-var body = $response.body
-    .replace(/<head>/, '<head><link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/ddgksf2013/Html/lezhu.css" type="text/css">')
-    .replace(/jquerys.js\?v/g, "ddgksf2013.js?v");
-$done({ body });
--- JS/zhenbuka.js
+++
@@ -1,2 +0,0 @@
-var body = $response.body.replace(/<head>/, '<head><link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/ddgksf2013/Html/zhenbuka.css" type="text/css">').replace(/'6572'/g, "'6578'");
-$done({ body });
