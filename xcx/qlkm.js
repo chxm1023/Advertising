351076467bbe9581257edd06fca5925bc592f900
chxm1023 <106084065+chxm1023@users.noreply.github.com>
2023-05-04 01:25:42 +0800

    Update qlkm.js

--- xcx/qlkm.js
+++ xcx/qlkm.js
@@ -9,7 +9,7 @@
 **************************************
 
 [rewrite_local]
-^https:\/\/(qadx.*\.qinlinad|mall-dsp.*\.qinlinkeji)\.com url script-response-body https://raw.githubusercontent.com/chxm1023/Advertising/main/qlkm.js
+^https:\/\/(qadx.*\.qinlinad|mall-dsp.*\.qinlinkeji)\.com url script-response-body https://raw.githubusercontent.com/chxm1023/Advertising/main/xcx/qlkm.js
 
 [mitm]
 hostname = qadx*.qinlinad.com, mall-dsp*.qinlinkeji.com
