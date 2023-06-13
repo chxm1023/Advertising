896160d72e7954f934b5878d9b05b1dfba2d4f1e
chxm1023 <106084065+chxm1023@users.noreply.github.com>
2023-06-13 19:11:01 +0800

    更新 quting.js

--- quting.js
+++ quting.js
@@ -1,21 +1,21 @@
 /****************************************
 
-项目功能：趣听音乐馆 屏蔽弹窗-自动激活App
-使用声明：仅供学习与交流，请勿转载与贩卖！⚠️⚠️⚠️
+项目功能：趣听音乐馆
+功能说明：屏蔽弹窗/首页横幅
+脚本作者：chxm1023
+电报频道：https://t.me/chxm1023
+使用声明：⚠️仅供参考，🈲转载与售卖！
 
 *****************************************
 
             
 [rewrite_local]
-
-^https?:\/\/api\.bspapp\.com\/client$ url script-response-body https://raw.githubusercontent.com/chxm1023/Advertising/main/quting.js
+^https?:\/\/(api|api\.next)\.bspapp\.com\/client$ url script-response-body https://raw.githubusercontent.com/chxm1023/Advertising/main/quting.js
 
 [mitm]
-
-hostname = api.bspapp.com
+hostname = api.bspapp.com, api.next.bspapp.com
 
 ****************************************/
 
 
-var body = $response.body.replace(/needVerify":true/g, 'needVerify":false').replace(/code":\d+/g, 'code":0');
-$done({ body });
+var body = $response.body.replace(/\"needVerify":\d+/g, '\"needVerify":false').replace(/\"bannerList":\[.+\]/g, '\"bannerList":[]');$done({body});
