bb6dd155f35df8f4dcf73c300c046bc61497f1f2
chxm1023 <106084065+chxm1023@users.noreply.github.com>
2023-06-23 14:56:13 +0800

    更新 sssp.js

--- sssp.js
+++ sssp.js
@@ -34,13 +34,13 @@ body = body.replace(/(<div class="banner mt-3">)[\s\S[\d\D]{0,20000}(<h3 class="
 
 body = body.replace(/(<div class="iconad">)[\s\S[\d\D]{0,20000}(<h3 class="mt-3">今日推荐<\/h3>)/g,'<h3 class="mt-3">今日推荐</h3>');
 
+body = body.replace(/(<div class="iconad">)[\s\S[\d\D]{0,20000}(<div class="row lists">)/g,'<div class="row lists">');
+
 body = body.replace(/(<div class="banner">)[\s\S[\d\D]{0,20000}(<div class="play mt-3">)/g,'<div class="play mt-3">');
 
-body = body.replace(/(<div class="banner">)[\s\S[\d\D]{0,2500}(<div class="row lists">)/g,'<div class="row lists">');
+body = body.replace(/(<div class="banner">)[\s\S[\d\D]{0,20000}(<div class="row lists">)/g,'<div class="row lists">');
 
 body = body.replace(/(<div class="banner mt-3">)[\s\S[\d\D]{0,20000}(<div class="mt-5 text-center">)/g,'<div class="mt-5 text-center">');
-
-body = body.replace(/(<div class="banner">)[\s\S[\d\D]{0,20000}(<div class="row lists">)/g,'<div class="row lists">');
 
 body = body.replace(/(<div class="float-app">)[\s\S[\d\D]{0,20000}(<div class="container">)/g,'<div class="container">');
 
